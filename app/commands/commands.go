018f13a32bf1a00e97a29b09c368d5e49b3ab47e
Andreas Pfohl <mail@apfohl.com>
2025-03-06 14:23:56 +0100

    added echo builtin

---
+++ app/commands/commands.go
@@ -0,0 +1,27 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Exit(args []string) {
+	if len(args) == 0 {
+		os.Exit(0)
+	}
+
+	code, err := strconv.Atoi(args[0])
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%s: could not be parsed\n", args[0])
+		os.Exit(-1)
+	}
+
+	os.Exit(code)
+}
+
+func Echo(args []string) {
+	_, _ = fmt.Fprintf(os.Stdout, "%s\n", strings.Join(args, " "))
+	os.Exit(0)
+}
--- app/main.go
+++ app/main.go
@@ -3,22 +3,36 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/codecrafters-io/shell-starter-go/app/commands"
 	"os"
 	"strings"
 )
 
 func main() {
+	commandMap := map[string]func([]string){
+		"exit": commands.Exit,
+		"echo": commands.Echo,
+	}
+
 	for {
-		fmt.Fprint(os.Stdout, "$ ")
+		_, _ = fmt.Fprint(os.Stdout, "$ ")
 
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		input = strings.Trim(input, "\n")
 
-		switch input {
-		case "exit 0":
-			os.Exit(0)
-		default:
-			_, _ = fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+		parts := strings.Split(input, " ")
+		cmd := parts[0]
+		var args []string
+		if len(parts) > 1 {
+			args = parts[1:]
 		}
+
+		command, ok := commandMap[cmd]
+		if !ok {
+			_, _ = fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+			os.Exit(-1)
+		}
+
+		command(args)
 	}
 }
